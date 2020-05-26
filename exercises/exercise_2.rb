8b37a88a813bd98ec1e90ade372b48518bb97f57
Angad Grewal <angadgrewal31@gmail.com>
2020-05-26 20:14:26 +0000

    completed ex_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Stores < ActiveRecord::Base
+end
+
+@store1 = Stores.where(id: 1)
+@store2 = Stores.where(id: 2)
+p @store1
+
+@store1.update(1, name: "Surrey")
+p @store1
